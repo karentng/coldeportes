1a97742788a5f27cd0b83cb77c32bd47d0f3127f
Daniel Correa <danielc.web@gmail.com>
2015-06-10 12:02:50 -0500

    Solucionados conflictos con migraciones, rama a nivel de master

---
+++ snd/migrations/0026_merge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('snd', '0006_auto_20150609_2057'),
+        ('snd', '0006_auto_20150610_1050'),
+        ('snd', '0025_auto_20150609_1247'),
+    ]
+
+    operations = [
+    ]
