6082c3fa8d871f98ed66fea08a4bf52d4ce2cdfd
Karent Nathalia Narvaez G <karentnathalia@gmail.com>
2015-07-09 12:55:42 -0500

    carga de datos iniciales servicios cajas de compensación

--- datos_iniciales/cajas_compensacion.sql
+++ datos_iniciales/cajas_compensacion.sql
@@ -3,3 +3,17 @@ INSERT INTO "entidades_tiposervicioescenariocajacompensacion" ("id", "descripcio
 (2,	'Masaje'),
 (3,	'Turco'),
 (4,	'Sauna');
+
+INSERT INTO "entidades_tiposerviciocajacompensacion" ("id", "categoria", "descripcion") VALUES
+(1,	'Social',	'Celebración de Fechas Especiales'),
+(2,	'Recreación',	'Programa para Adultos'),
+(3,	'Recreación',	'Programa para Discapacitados'),
+(4,	'Recreación',	'Programa Recreativos'),
+(5,	'Recreación',	'Vacaciones Recreativas'),
+(6,	'Turismo',	'Planes'),
+(7,	'Deportivos',	'Vacaciones Deportivas'),
+(8,	'Deportivos',	'Escuelas Deportivas'),
+(9,	'Deportivos',	'Juegos Interempresariales'),
+(10,	'Programas Especiales',	'Madres Gestantes'),
+(11,	'Programas Especiales',	'Eventos Empresariales'),
+(12,	'Programas Especiales',	'Gimnasia Especializada');
