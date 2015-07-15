532f07a789c5a5edb5f24dd570b7b807a8909fe7
Andrés Camilo Serna <andres.c.serna@gmail.com>
2015-07-15 00:42:49 -0500

    Creacion de tenants con actores del sistema y validaciones de menu (CAF); Asignar permisos al grupo Digitador automaticamente (en la creacion)

--- entidades/views.py
+++ entidades/views.py
@@ -13,24 +13,31 @@ def tipo(request):
     })
 
 @login_required
-def registro(request):
+def registro(request, tipo):
     form = EntidadForm()
+    form2 = ActoresForm()
+
     dominio = settings.SUBDOMINIO_URL
 
     if request.method == 'POST':
         form = EntidadForm(request.POST)
-        
-        if form.is_valid():
+        form2 = ActoresForm(request.POST)
+        if form.is_valid() and form2.is_valid():
+            actores = form2.save()
+
             pagina = form.cleaned_data['pagina']
             obj = form.save(commit=False)
             obj.schema_name = pagina
             obj.domain_url = pagina + dominio
+            obj.actores = actores
+            obj.tipo = tipo
             obj.save()
 
             messages.success(request, "Entidad registrada correctamente.")
-            return redirect('entidad_registro')
+            return redirect('entidad_registro', tipo)
 
     return render(request, 'entidad_registro.html', {
         'form': form,
+        'form2': form2,
         'dominio': dominio,
     })
