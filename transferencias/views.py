bcc68a5cf789f4a32a59381090cf8e7793104078
Daniel Correa <danielc.web@gmail.com>
2015-07-16 20:54:20 -0500

    Generacion de transferencias funcionando correctamente

--- transferencias/views.py
+++ transferencias/views.py
@@ -1,12 +1,13 @@
 from django.contrib.contenttypes.models import ContentType
 from django.db import connection
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.contrib import messages
 from entidades.models import Entidad
 from snd.models import Deportista,Escenario,Entrenador
-from .forms import TransferenciaForm
+from .models import Transferencia
+import datetime
 # Create your views here.
 @login_required
 def generar_transferencia(request,tipo_transfer,tipo_persona,id):
@@ -29,21 +30,31 @@ def generar_transferencia(request,tipo_transfer,tipo_persona,id):
     objeto = None
     entidad_solicitante = request.tenant
     entidades = Entidad.objects.exclude(nombre__in=['publico',entidad_solicitante.nombre])
-    if tipo_transfer==1: #Transferencia de personas
-        if tipo_persona==1: #Transferencia de deportistas
+    tipo_objeto = ""
+    if tipo_transfer=='1': #Transferencia de personas
+        if tipo_persona=='1': #Transferencia de deportistas
             objeto = Deportista.objects.get(id=id)
+            tipo_objeto='Deportista'
         elif tipo_persona==2: #Transferencia de entrenadores
             objeto = Entrenador.objects.get(id=id)
+            tipo_objeto='Entrenador'
     elif tipo_transfer==2: #Transferencia de escenarios
         objeto = Escenario.objects.get(id=id)
+        tipo_objeto='Escenario'
 
     if request.method == 'POST':
-        #trans_form = TransferenciaForm(request.POST)
-        #entidad_cambio = trans_form.cleaned_data['entidad']
-        #print(entidad_cambio)
-        #connection.set_tenant(entidad)
-        #ContentType.objects.clear_cache()
-        pass
+        id_entidad_cambio = request.POST['entidad']
+        entidad_cambio = Entidad.objects.get(id=id_entidad_cambio)
+        connection.set_tenant(entidad_cambio)
+        ContentType.objects.clear_cache()
+        transferencia = Transferencia()
+        transferencia.entidad = entidad_solicitante
+        transferencia.fecha_solicitud = datetime.date.today()
+        transferencia.id_objeto = objeto.id
+        transferencia.tipo_objeto = tipo_objeto
+        transferencia.save()
+        messages.success(request,'Transferencia generada exitosamente, se le informara cuando la entidad acepte su solicitud')
+        return redirect('deportista_listar')
 
     return render(request,'generar_transferencia.html',{
         'entidades' : entidades,
