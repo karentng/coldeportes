56c3f007ab2b8a8a8ce5e5b8498d5138cad19ac8
Daniel Correa <danielc.web@gmail.com>
2015-07-27 12:10:35 -0500

    Validaciones para historial deportivo terminadas

--- static/js/validaciones/deportistas/wizard-deportiva-validations.js
+++ static/js/validaciones/deportistas/wizard-deportiva-validations.js
@@ -36,6 +36,11 @@ fields = {
                     numeric: {
                         message: 'El puesto debe ser numérico'
                     },
+                    greaterThan: {
+                        message: 'El puesto no puede ser menor o igual a 0',
+                        value: 1
+
+                    }
                 }
             },
             categoria: {
@@ -44,7 +49,39 @@ fields = {
                         message: 'La categoría no puede ser vaciá'
                     },
                 }
+            },
+            fecha_inicial: {
+                validators: {
+                    notEmpty: {
+                        message: 'La fecha de inicio de campeonato no puede ser vacía'
+                    },
+                    date: {
+                        message: 'El valor ingresado no es una fecha válida, verifique que no sea mayor a la de finalización',
+                        format: 'YYYY-MM-DD',
+                        max: 'fecha_final'
+                    }
+                }
+            },
+            fecha_final: {
+                validators: {
+                    date: {
+                        message: 'El valor ingresado no es una fecha válida',
+                        format: 'YYYY-MM-DD',
+                        min: 'fecha_inicial'
+                    }
+                }
             }
 
         };
+//Revalidar campos al ser actualizados
+    $("#id_fecha_inicial").on('change',function(e){
+        $(form).bootstrapValidator('revalidateField', 'fecha_inicial');
+        $(form).bootstrapValidator('revalidateField', 'fecha_final');
+    });
+//Revalidar campos al ser actualizados
+    $("#id_fecha_final").on('change',function(e){
+        $(form).bootstrapValidator('revalidateField', 'fecha_inicial');
+        $(form).bootstrapValidator('revalidateField', 'fecha_final');
+    });
+
 $.getScript(base+"js/validaciones/validations-base.js");
