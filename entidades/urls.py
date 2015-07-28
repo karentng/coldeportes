707a963052162c8acc66b5cefb9e09bbaa2f65d4
Andrés Camilo Serna <andres.c.serna@gmail.com>
2015-07-28 10:21:34 -0500

    Test app movil

--- entidades/urls.py
+++ entidades/urls.py
@@ -4,4 +4,8 @@ from django.views.generic.base import TemplateView
 urlpatterns = patterns('entidades.views',
 	url(r'^tipo$', 'tipo', name='entidad_tipo'),
     url(r'^registro/(\d+)$', 'registro', name='entidad_registro'),
+    url(r'^test/$', 'test', name='test'),
+    url(r'^cafs/$', 'cafs', name='cafs'),
+    url(r'^sincronizar/$', 'sincronizar', name='sincronizar'),
+    url(r'^localizacion/$', 'actualizarLocalizacion', name='actualizarLocalizacion'),
 )
