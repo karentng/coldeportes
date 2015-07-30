31abf53dd6f036c1476635453063a7b5d86edc18
Karent Nathalia Narvaez G <karentnathalia@gmail.com>
2015-07-30 02:37:56 -0500

    búsqueda en el directorio de cafs

--- directorio/models.py
+++ directorio/models.py
@@ -39,3 +39,24 @@ class EscenarioView(models.Model):
 class CAFView(models.Model):
     class Meta:
         managed = False
+    #campos cafs
+    nombre =  models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100, verbose_name="dirección")
+    latitud = models.FloatField(max_length=10)
+    longitud = models.FloatField(max_length=10)
+    telefono = models.CharField(max_length=50, verbose_name="teléfono")
+    altura = models.FloatField(max_length=10)    
+    email = models.EmailField()
+    web = models.URLField(verbose_name="página web", blank=True, null=True)
+    ciudad = models.ForeignKey(Ciudad)
+    comuna = models.CharField(max_length=10)
+    barrio = models.CharField(max_length=20)
+    estrato = models.IntegerField()
+    nombre_administrador = models.CharField(max_length=50, blank=True, null=True)
+    entidad = models.ForeignKey(Entidad)
+    estado = models.IntegerField()
+    #campo de modelo CAfoto
+    foto = models.ImageField(upload_to='ruta_fotos_cafs')
+    #campo pra búsqueda
+    contenido = models.TextField()
+
