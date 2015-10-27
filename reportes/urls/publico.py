bb0fa3f733d101a8427a4d1755bfc595c4f579ce
Andrés Camilo Serna <andres.c.serna@gmail.com>
2015-10-27 17:02:05 -0500

    version 0.01 reportes caf

--- reportes/urls/publico.py
+++ reportes/urls/publico.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('reportes.views.publico',
     url(r'^tipos$', 'tipos', name='reportes_publico_tipos'),
-    #url(r'caf/', include('reportes.urls.caf')),
+    url(r'caf/', include('reportes.urls.caf')),
 )
