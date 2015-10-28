c05b93f032492ee03386dca79e57ce7fa13f6a07
Andrés Camilo Serna <andres.c.serna@gmail.com>
2015-10-28 10:47:31 -0500

    modificar escenarios en liga si son propios

--- snd/modelos_de_datos.py
+++ snd/modelos_de_datos.py
@@ -368,6 +368,17 @@ MODELOS_DE_DATOS = (
                 'fa-eye',
                 None
             ],
+            [
+                "Editar",
+                'wizard_identificacion',
+                ['id'],
+                'fa-gear',
+                [
+                    [
+                        ['entidad'],
+                    ]
+                ]
+            ],
         ],
     ),
     (   #Numero de modelo 10
