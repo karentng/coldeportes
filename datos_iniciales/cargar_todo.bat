e4d4af8f818adcadbdfefc7278bf191e82e8c887
Cristian Leonardo Ríos López <cristian.rios@correounivalle.edu.co>
2015-11-12 09:10:48 -0500

    se agregan datos iniciales escuelas deportivas y centros biomedicos

--- datos_iniciales/cargar_todo.bat
+++ datos_iniciales/cargar_todo.bat
@@ -3,3 +3,5 @@ python ../manage.py dbshell < tipo_superficie.sql
 python ../manage.py dbshell < datos_iniciales_cafs.sql
 python ../manage.py dbshell < cajas_compensacion.sql
 python ../manage.py dbshell < datos_eps.sql
+python ../manage.py dbshell < datos_iniciales_centros_biomedicos.sql
+python ../manage.py dbshell < datos_iniciales_escuelas_deportivas.sql
