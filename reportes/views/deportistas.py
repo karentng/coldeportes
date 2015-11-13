a124956b6c2d714cca83f1e6c19c1b6ea4e9f913
Daniel Correa <danielc.web@gmail.com>
2015-11-13 15:18:41 +0100

    Reporte de cantidad de deportistas con beneficio de programa de apoyo con etiquetas true, false

--- reportes/views/deportistas.py
+++ reportes/views/deportistas.py
@@ -1,6 +1,6 @@
 #encoding:utf-8
 from django.shortcuts import render, redirect
-from snd.models import HistorialDeportivo,Deportista
+from snd.models import HistorialDeportivo,Deportista,InformacionAdicional
 from entidades.models import Departamento
 from django.db.models import Count
 from reportes.forms import FiltrosDeportistasForm
@@ -10,6 +10,9 @@ from django.http import JsonResponse
 
 def ejecutar_casos_recursivos(consultas,departamentos,genero,tipoTenant):
     """
+    Noviembre 13, 2015
+    Autor: Daniel Correa
+
     Permite ejecutar los diferentes filtros de casos de acuerdo a un arreglo de consultas
     CONSULTAS LLEVA EL SIGUIENTE FORMATO [contulta caso 1, consulta caso 2 , consulta caso 3, ... ,consulta caso n]
     LOS CASOS EMPIEZAN EN 1 EL DE MAS ARRIBA HASTA N EL DE MAS ABAJO
@@ -27,6 +30,9 @@ def ejecutar_casos_recursivos(consultas,departamentos,genero,tipoTenant):
 
 def participaciones_deportivas(request):
     """
+    Noviembre 13, 2015
+    Autor: Daniel Correa
+
     Reporte participaciones deportivas:
     Consulta que trae el numero de  participaciones deportiva ordenadas por tipo
     """
@@ -36,10 +42,10 @@ def participaciones_deportivas(request):
         genero = None if request.GET['genero'] == 'null'  else ast.literal_eval(request.GET['genero'])
 
         consultas = [
-            "list(HistorialDeportivo.objects.filter(deportista__ciudad_residencia__departamento__id__in=%s,deportista__genero__in=%s).annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))",
-            "list(HistorialDeportivo.objects.filter(deportista__ciudad_residencia__departamento__id__in=%s).annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))",
-            "list(HistorialDeportivo.objects.filter(deportista__genero__in=%s).annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))",
-            "list(HistorialDeportivo.objects.annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))"
+            "list(HistorialDeportivo.objects.filter(deportista__estado = 0,deportista__ciudad_residencia__departamento__id__in=%s,deportista__genero__in=%s).annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))",
+            "list(HistorialDeportivo.objects.filter(deportista__estado = 0,deportista__ciudad_residencia__departamento__id__in=%s).annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))",
+            "list(HistorialDeportivo.objects.filter(deportista__estado = 0,deportista__genero__in=%s).annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))",
+            "list(HistorialDeportivo.objects.filter(deportista__estado = 0,).annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))"
 
 
         ]
@@ -50,7 +56,45 @@ def participaciones_deportivas(request):
 
     else:
         #Traer la cantidad de hisotriales ordenados por tipo
-        participaciones = tipoTenant.ejecutar_consulta(True, "list(HistorialDeportivo.objects.annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))")
+        participaciones = tipoTenant.ejecutar_consulta(True, "list(HistorialDeportivo.objects.filter(deportista__estado = 0,).annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))")
+
+    visualizaciones = [1, 2, 3]
+    form = FiltrosDeportistasForm(visualizaciones=visualizaciones)
+    return render(request, 'deportistas/reportes_deportistas.html', {
+        'nombre_reporte' : 'Participaciones Deportivas',
+        'url_data' : 'reporte_participaciones_deportivas',
+        'datos': participaciones,
+        'visualizaciones': visualizaciones,
+        'form': form,
+    })
+
+
+def beneficiario_programa_apoyo(request):
+    """
+    Noviembre 13, 2015
+    Autor: Daniel Correa
+
+    Permite conocer el numero de deportistas beneficiados por un programa de apoyo
+    """
+    tipoTenant = request.tenant.obtenerTenant()
+    if request.is_ajax():
+        departamentos = None if request.GET['departamentos'] == 'null'  else ast.literal_eval(request.GET['departamentos'])
+        genero = None if request.GET['genero'] == 'null'  else ast.literal_eval(request.GET['genero'])
+
+        consultas = [
+            "list(InformacionAdicional.objects.filter(deportista__estado = 0,deportista__ciudad_residencia__departamento__id__in=%s,deportista__genero__in=%s).annotate(descripcion=F('es_beneficiario_programa_apoyo')).values('descripcion').annotate(cantidad=Count('es_beneficiario_programa_apoyo')))",
+            "list(InformacionAdicional.objects.filter(deportista__estado = 0,deportista__ciudad_residencia__departamento__id__in=%s).annotate(descripcion=F('es_beneficiario_programa_apoyo')).values('descripcion').annotate(cantidad=Count('es_beneficiario_programa_apoyo')))",
+            "list(InformacionAdicional.objects.filter(deportista__estado = 0,deportista__genero__in=%s).annotate(descripcion=F('es_beneficiario_programa_apoyo')).values('descripcion').annotate(cantidad=Count('es_beneficiario_programa_apoyo')))",
+            "list(InformacionAdicional.objects.filter(deportista__estado = 0).annotate(descripcion=F('es_beneficiario_programa_apoyo')).values('descripcion').annotate(cantidad=Count('es_beneficiario_programa_apoyo')))"
+        ]
+
+        participaciones = ejecutar_casos_recursivos(consultas,departamentos,genero,tipoTenant)
+
+        return JsonResponse(participaciones)
+
+    else:
+        #Traer la cantidad de hisotriales ordenados por tipo
+        participaciones = tipoTenant.ejecutar_consulta(True, "list(InformacionAdicional.objects.filter(deportista__estado = 0).annotate(descripcion=F('es_beneficiario_programa_apoyo')).values('descripcion').annotate(cantidad=Count('es_beneficiario_programa_apoyo')))")
 
     visualizaciones = [1, 2, 3]
     form = FiltrosDeportistasForm(visualizaciones=visualizaciones)
