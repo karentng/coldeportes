d3808f2d5f6a0302ec33134f3f3203727971358b
Daniel Correa <danielc.web@gmail.com>
2015-11-13 11:11:02 +0100

    Reporte pariticipanes deportivas sin ajax funcionando

--- reportes/forms.py
+++ reportes/forms.py
@@ -35,4 +35,21 @@ class FiltrosDeportistasForm(forms.Form):
     """
     Formulario para filtros de deportistas
     """
-    pass
+    def __init__(self, *args, **kwargs):
+        visualizaciones_definidas = kwargs.pop('visualizaciones', None)
+        super(FiltrosDeportistasForm, self).__init__(*args, **kwargs)
+        self.fields['departamento'] = adicionarClase(self.fields['departamento'], 'many')
+        self.fields['visualizacion'] = adicionarClase(self.fields['visualizacion'], 'one')
+        self.fields['genero'] = adicionarClase(self.fields['genero'], 'many')
+
+        visualizaciones = tuple()
+        if visualizaciones_definidas:
+            for i in self.fields['visualizacion'].choices:
+                if i[0] in visualizaciones_definidas:
+                    visualizaciones += (i,)
+            self.fields['visualizacion'].choices = visualizaciones
+
+
+    departamento = forms.ModelMultipleChoiceField(queryset=Departamento.objects.all(), required=False)
+    genero = forms.MultipleChoiceField(choices=(('HOMBRE','HOMBRE'),('MUJER','MUJER'),),required=False, label="Genero")
+    visualizacion = forms.ChoiceField(choices=VISUALIZACIONES)
--- reportes/views/deportistas.py
+++ reportes/views/deportistas.py
@@ -1,21 +1,28 @@
 #encoding:utf-8
 from django.shortcuts import render, redirect
-from django.db import models
-from django.http import JsonResponse
-import ast
+from snd.models import HistorialDeportivo
+from django.db.models import Count
+from reportes.forms import FiltrosDeportistasForm
+from django.db.models import F
 
 def participaciones_deportivas(request):
     """
-    Reporte participaciones deportivas
+    Reporte participaciones deportivas:
+    Consulta que trae el numero de  participaciones deportiva ordenadas por tipo
     """
-    #consulta que trae todas las participaciones deportiva
-    '''
-    {"internacional":20,...}
-    []
-
-    '''
     tipoTenant = request.tenant.obtenerTenant()
     if request.is_ajax():
         pass
     else:
-        pass
+        #Traer la cantidad de hisotriales ordenados por tipo
+        centros = tipoTenant.ejecutar_consulta(True, "list(HistorialDeportivo.objects.annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo')))")
+        consulta = HistorialDeportivo.objects.annotate(descripcion=F('tipo')).values('descripcion').annotate(cantidad=Count('tipo'))
+        print(consulta)
+
+    visualizaciones = [1, 2, 3]
+    form = FiltrosDeportistasForm(visualizaciones=visualizaciones)
+    return render(request, 'deportistas/participaciones_deportivas.html', {
+        'centros': centros,
+        'visualizaciones': visualizaciones,
+        'form': form,
+    })
