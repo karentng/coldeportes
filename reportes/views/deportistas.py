f3817c33222f931448a1b5fede191d7581f58ddc
Daniel Correa <danielc.web@gmail.com>
2015-11-13 16:32:38 +0100

    Reporte etnias de los deportistas

--- reportes/views/deportistas.py
+++ reportes/views/deportistas.py
@@ -1,6 +1,6 @@
 #encoding:utf-8
 from django.shortcuts import render, redirect
-from snd.models import HistorialDeportivo,Deportista,InformacionAdicional
+from snd.models import HistorialDeportivo,Deportista,InformacionAdicional,Deportista
 from entidades.models import Departamento
 from django.db.models import Count
 from reportes.forms import FiltrosDeportistasForm
@@ -88,20 +88,55 @@ def beneficiario_programa_apoyo(request):
             "list(InformacionAdicional.objects.filter(deportista__estado = 0).annotate(descripcion=F('es_beneficiario_programa_apoyo')).values('descripcion').annotate(cantidad=Count('es_beneficiario_programa_apoyo')))"
         ]
 
-        participaciones = ejecutar_casos_recursivos(consultas,departamentos,genero,tipoTenant)
+        beneficiados = ejecutar_casos_recursivos(consultas,departamentos,genero,tipoTenant)
 
-        return JsonResponse(participaciones)
+        return JsonResponse(beneficiados)
 
     else:
-        #Traer la cantidad de hisotriales ordenados por tipo
-        participaciones = tipoTenant.ejecutar_consulta(True, "list(InformacionAdicional.objects.filter(deportista__estado = 0).annotate(descripcion=F('es_beneficiario_programa_apoyo')).values('descripcion').annotate(cantidad=Count('es_beneficiario_programa_apoyo')))")
+        beneficiados = tipoTenant.ejecutar_consulta(True, "list(InformacionAdicional.objects.filter(deportista__estado = 0).annotate(descripcion=F('es_beneficiario_programa_apoyo')).values('descripcion').annotate(cantidad=Count('es_beneficiario_programa_apoyo')))")
 
     visualizaciones = [1, 2, 3]
     form = FiltrosDeportistasForm(visualizaciones=visualizaciones)
     return render(request, 'deportistas/reportes_deportistas.html', {
-        'nombre_reporte' : 'Participaciones Deportivas',
-        'url_data' : 'reporte_participaciones_deportivas',
-        'datos': participaciones,
+        'nombre_reporte' : 'Beneficiario Programa de Apoyo',
+        'url_data' : 'reporte_beneficiario_programa_apoyo',
+        'datos': beneficiados,
+        'visualizaciones': visualizaciones,
+        'form': form,
+    })
+
+def etinias_deportistas(request):
+    """
+    Noviembre 13, 2015
+    Autor: Daniel Correa
+
+    Permite conocer el numero de deportistas ordenados por etnias
+    """
+    tipoTenant = request.tenant.obtenerTenant()
+    if request.is_ajax():
+        departamentos = None if request.GET['departamentos'] == 'null'  else ast.literal_eval(request.GET['departamentos'])
+        genero = None if request.GET['genero'] == 'null'  else ast.literal_eval(request.GET['genero'])
+
+        consultas = [
+            "list(Deportista.objects.filter(estado=0,ciudad_residencia__departamento__id__in=%s,genero__in=%s).annotate(descripcion=F('etnia')).values('descripcion').annotate(cantidad=Count('etnia')))",
+            "list(Deportista.objects.filter(estado=0,ciudad_residencia__departamento__id__in=%s).annotate(descripcion=F('etnia')).values('descripcion').annotate(cantidad=Count('etnia')))",
+            "list(Deportista.objects.filter(estado=0,genero__in=%s).annotate(descripcion=F('etnia')).values('descripcion').annotate(cantidad=Count('etnia')))",
+            "list(Deportista.objects.filter(estado=0).annotate(descripcion=F('etnia')).values('descripcion').annotate(cantidad=Count('etnia')))",
+        ]
+
+        etnias = ejecutar_casos_recursivos(consultas,departamentos,genero,tipoTenant)
+
+        return JsonResponse(etnias)
+
+    else:
+        etnias = tipoTenant.ejecutar_consulta(True, "list(Deportista.objects.filter(estado=0).annotate(descripcion=F('etnia')).values('descripcion').annotate(cantidad=Count('etnia')))")
+
+    visualizaciones = [1, 2, 3]
+    form = FiltrosDeportistasForm(visualizaciones=visualizaciones)
+    return render(request, 'deportistas/reportes_deportistas.html', {
+        'nombre_reporte' : 'Etnias de los deportistas',
+        'url_data' : 'reporte_etinias_deportistas',
+        'datos': etnias,
         'visualizaciones': visualizaciones,
         'form': form,
     })
