ea28d0e0e48d4657a32c92434c7f7c28eb9b2fb1
Daniel Correa <danielc.web@gmail.com>
2015-12-20 21:27:17 +0100

    Filtros personal de apoyo

--- reportes/views/personal_apoyo.py
+++ reportes/views/personal_apoyo.py
@@ -7,6 +7,21 @@ from reportes.forms import FiltrosPersonalApoyoForm
 from django.db.models import F, Count
 from reportes.utilities import sumar_datos_diccionario, convert_choices_to_array, crear_diccionario_inicial
 from snd.models import PersonalApoyo
+import ast
+
+
+def ejecutar_casos_recursivos(consultas,departamentos,genero,tipoTenant):
+
+    if departamentos and genero:
+        participaciones = eval(consultas[0]%(departamentos,genero))
+    elif departamentos:
+        participaciones = eval(consultas[1]%(departamentos))
+    elif genero:
+        participaciones = eval(consultas[2]%(genero))
+    else:
+        participaciones = eval(consultas[3])
+    participaciones = tipoTenant.ajustar_resultado(participaciones)
+    return participaciones
 
 
 def reporte_actividades_personal(request):
@@ -25,13 +40,22 @@ def reporte_actividades_personal(request):
     else:
         tabla = TenantPersonalApoyoView
 
-
-
     if request.is_ajax():
-        datos = list(tabla.objects.annotate(descripcion=F('actividad')).values('id','descripcion').annotate(cantidad=Count('descripcion', distinct=True)))
-        datos = tipoTenant.ajustar_resultado(datos)
+
+        departamentos = None if request.GET['departamentos'] == 'null'  else ast.literal_eval(request.GET['departamentos'])
+        genero = None if request.GET['genero'] == 'null'  else ast.literal_eval(request.GET['genero'])
+
+        consultas = [
+            "list("+tabla.__name__+".objects.filter(ciudad__departamento__id__in=%s,genero__in=%s).annotate(descripcion=F('actividad')).values('id','descripcion').annotate(cantidad=Count('descripcion', distinct=True)))",
+            "list("+tabla.__name__+".objects.filter(ciudad__departamento__id__in=%s).annotate(descripcion=F('actividad')).values('id','descripcion').annotate(cantidad=Count('descripcion', distinct=True)))",
+            "list("+tabla.__name__+".objects.filter(genero__in=%s).annotate(descripcion=F('actividad')).values('id','descripcion').annotate(cantidad=Count('descripcion', distinct=True)))",
+            "list("+tabla.__name__+".objects.annotate(descripcion=F('actividad')).values('id','descripcion').annotate(cantidad=Count('descripcion', distinct=True)))",
+        ]
+
+        datos = ejecutar_casos_recursivos(consultas,departamentos,genero,tipoTenant)
 
         return JsonResponse(datos)
+
     else:
         datos = list(tabla.objects.annotate(descripcion=F('actividad')).values('id','descripcion').annotate(cantidad=Count('descripcion', distinct=True)))
         #datos = tipoTenant.ajustar_resultado(datos)
