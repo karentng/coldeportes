70256410a27136a37e27f10f380743c9da503ee9
Karent Nathalia Narvaez G <karentnathalia@gmail.com>
2016-01-18 16:09:14 -0500

    issue 345: se adiciona opciones de choices a generador de reportes para corregir opciones en gráfica

--- reportes/views/escenarios.py
+++ reportes/views/escenarios.py
@@ -40,6 +40,27 @@ def verificar_seleccion_reporte(opcion_reporte):
 
     return categoria
 
+def obtener_choices(opcion_reporte):
+    clases_choices = CaracterizacionEscenario.ACCESOS
+    if opcion_reporte == 'ES':
+        clases_choices = Escenario.estratos
+    elif opcion_reporte == 'CA':
+        clases_choices = CaracterizacionEscenario.ACCESOS
+    elif opcion_reporte == 'DT':
+        clases_choices = None
+    elif opcion_reporte == 'EF':
+        clases_choices = CaracterizacionEscenario.ESTADOS_FISICOS
+    elif opcion_reporte == 'TE':
+        clases_choices = None
+    elif opcion_reporte == 'TS':
+        clases_choices = None
+    elif opcion_reporte == 'TP':
+        clases_choices = CaracterizacionEscenario.PROPIETARIOS
+    elif opcion_reporte == 'CE':
+        clases_choices = None
+
+    return clases_choices
+
 
 def ejecutar_consulta_segun_filtro(categoria, cantidad, departamentos,municipios, disciplinas,tipoTenant, tabla, choices):
     """
@@ -103,6 +124,8 @@ def generador_reporte_escenario(request, tabla, cantidad, categoria=None, choice
     if not categoria:
     #si categoria es none es el reporte características escenarios
         categoria = verificar_seleccion_reporte(reporte)
+
+    choices = obtener_choices(reporte)
     escenarios = ejecutar_consulta_segun_filtro(categoria, cantidad, departamentos, municipios, disciplinas, tipoTenant, tabla, choices)
 
     if '' in escenarios:
