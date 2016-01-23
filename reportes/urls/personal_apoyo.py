1ddcef5ee2718c431f2521c6721b3b6b9bf3ad5c
Cristian Leonardo Ríos López <cristian.rios@correounivalle.edu.co>
2016-01-23 11:46:02 -0300

    se agrega reporte etnias del personal de apoyo

--- reportes/urls/personal_apoyo.py
+++ reportes/urls/personal_apoyo.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('reportes.views.personal_apoyo',
     url(r'^formacion-academica', 'reporte_formacion_academica_personal', name='reporte_formacion_academica_personal'),
     url(r'^lgtbi', 'reporte_lgtbi', name='reporte_lgtbi_personal_apoyo'),
     url(r'^cantidad-total-personal-apoyo', 'reporte_cantidad_total_personal_apoyo', name='reporte_cantidad_total_personal_apoyo'),
+    url(r'^etnias', 'reporte_etnias', name='reporte_etnias_personal_apoyo'),
 )
