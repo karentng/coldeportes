058f10a1ee1910bb9cf78625e6eeacbb2c805610
Daniel Correa <danielc.web@gmail.com>
2016-01-27 23:29:18 +0100

    Issue #381 resuelto, tildes y sugerencias implementadas

--- reportes/formularios/deportistas.py
+++ reportes/formularios/deportistas.py
@@ -64,7 +64,7 @@ class FiltrosDeportistasCategoriaForm(forms.Form):
         self.fields['reporte'].choices = self.TIPO_REPORTE
 
     departamento = forms.ModelMultipleChoiceField(queryset=Departamento.objects.all(), required=False)
-    genero = forms.MultipleChoiceField(choices=(('HOMBRE','MASCULINO'),('MUJER','FEMENINO'),),required=False, label="Genero")
+    genero = forms.MultipleChoiceField(choices=(('HOMBRE','MASCULINO'),('MUJER','FEMENINO'),),required=False, label="Género")
     #disciplina = forms.ModelMultipleChoiceField(queryset=TipoDisciplinaDeportiva.objects.all(), required=False)
-    reporte = forms.ChoiceField(label="Clasificar por",required=False,choices=TIPO_REPORTE)
-    visualizacion = forms.ChoiceField(choices=VISUALIZACIONES)
+    reporte = forms.ChoiceField(label="Clasificar por:",required=False,choices=TIPO_REPORTE)
+    visualizacion = forms.ChoiceField(choices=VISUALIZACIONES,label='Visualización')
