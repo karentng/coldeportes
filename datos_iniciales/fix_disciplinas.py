07cbfd04f30f3d10d2a3e2471553811aff9ea6b8
MiltonLn <miltonln04@gmail.com>
2016-01-29 16:07:29 -0500

    Fix listados nacionales

---
+++ datos_iniciales/fix_disciplinas.py
@@ -0,0 +1,4 @@
+from django.db import connection
+from entidades.models import Entidad,Federacion,ModalidadDisciplinaDeportiva,Liga,Club,EscuelaDeportiva
+from snd.modelos.deportistas import Deportista
+from snd.modelos.escenarios import CaracterizacionEscenario
