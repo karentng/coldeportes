e939ff8fe6feea6a6ecb0e3c3415e26fdb9d9872
MiltonLn <miltonln04@gmail.com>
2016-01-29 17:07:18 -0500

    Se acomodan menús de reportes

--- datos_iniciales/fix_disciplinas.py
+++ datos_iniciales/fix_disciplinas.py
@@ -1,4 +1,17 @@
 from django.db import connection
-from entidades.models import Entidad,Federacion,ModalidadDisciplinaDeportiva,Liga,Club,EscuelaDeportiva
+from entidades.models import Entidad,Federacion,ModalidadDisciplinaDeportiva,Liga,Club,EscuelaDeportiva,TipoDisciplinaDeportiva
 from snd.modelos.deportistas import Deportista
 from snd.modelos.escenarios import CaracterizacionEscenario
+
+
+
+entidades = Entidad.objects.exclude(schema_name='public')
+#DISCIPLINA A BORRAR TRIATHLON
+triathlon = TipoDisciplinaDeportiva.objects.get(id=82)
+#DISCIPLINA CORRECTA
+triatlon = TipoDisciplinaDeportiva.objects.get(id=42)
+
+
+for entidad in entidades:
+    connection.set_tenant(entidad)
+    federaciones = Federacion.objects.filter(disciplina__id=82).update(disciplina=42)
