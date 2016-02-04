632b1a368da601a9093093270a746a2091cd9d2c
Karent Nathalia Narvaez G <karentnathalia@gmail.com>
2016-02-04 10:03:32 -0500

    manual de usuarios catalogo

--- manual/urls.py
+++ manual/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('manual.views',
     url(r'^nueva-entrada$', 'nueva_entrada', name='nueva_entrada_manual'),
+    url(r'^ver-articulo$', 'ver_articulo', name='ver_articulo_manual'),
 )
