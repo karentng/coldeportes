c0242eb2b89da3aa6b17932c5ceec7957c3e2062
Karent Nathalia Narvaez <karentnathalia@gmail.com>
2016-02-09 22:39:48 -0500

    visualizador de manual en forma de arbol

--- manual/urls.py
+++ manual/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('manual.views',
     url(r'^nueva-entrada$', 'nueva_entrada', name='nueva_entrada_manual'),
-    #url(r'^ver-articulos$', 'ver_articulos', name='ver_articulos_manual'),
-    #url(r'^ver-articulo/(\d+)$', 'ver_articulo', name='ver_articulo_manual'),
+    url(r'^listar/(\d+)$', 'listar_articulo', name='listar_articulo_manual'),
+    url(r'^listar/$', 'listar', name='listar_manual'),
 )
