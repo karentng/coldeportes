4ff97cbd949f10b150d65a500a4f3757d84c59b1
Daniel Correa <danielc.web@gmail.com>
2016-02-17 16:27:09 +0100

    app de solicitud de escenarios

---
+++ solicitudes_escenarios/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class SolicitudEscenario(models.Model):
+    pass
+
+class DiscucionSolicitud(models.Model):
+    pass
+
+class ListaSolicitudes(models.Model):
+    pass
