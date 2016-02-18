d598fad9ba271a34dd26b1568b5a3b239b7e5c0d
Daniel Correa <danielc.web@gmail.com>
2016-02-18 23:06:37 +0100

    modelos iniciales adecuacion escenarios

--- solicitudes_escenarios/models.py
+++ solicitudes_escenarios/models.py
@@ -1,11 +1,47 @@
 from django.db import models
-
+from snd.models import Escenario
+from entidades.models import Entidad
 # Create your models here.
 class SolicitudEscenario(models.Model):
-    pass
+    TIPOS= ()
+    PRIORIDADES = (
+        (0,'BAJA'),
+        (1,'MEDIA'),
+        (2,'ALTA'),
+    )
+    ESTADOS = (
+        (0,'ESPERANDO RESPUESTA'),
+        (1,'INCOMPLETA'),
+        (2,'APROBADA'),
+        (3,'ANULADA')
+    )
+
+    escenarios = models.ManyToManyField(Escenario)
+    tipo = models.IntegerField(choices=TIPOS)
+    prioridad = models.IntegerField(choices=PRIORIDADES)
+    descripcion = models.TextField()
+    para_quien = models.ForeignKey(Entidad)
+
+    def codigo_unico(self):
+        return ("AD-%s-%s")%(self.para_quien.id,self.id)
+
+class AdjuntoSolicitud(models.Model):
+    solicitud = models.ForeignKey(SolicitudEscenario)
+    archivo = models.FileField(upload_to="solicitudes_escenarios")
 
 class DiscucionSolicitud(models.Model):
-    pass
+    ESTADOS = (
+        (0,'ESPERANDO RESPUESTA'),
+        (1,'INCOMPLETA'),
+        (2,'APROBADA'),
+        (3,'ANULADA')
+    )
+
+    estado_anterior = models.IntegerField(choices=ESTADOS)
+    estado_actual = models.IntegerField(choices=ESTADOS)
+    descripcion = models.TextField()
+    solicitud = models.ForeignKey(SolicitudEscenario)
 
 class ListaSolicitudes(models.Model):
-    pass
+    entidad_solicitante = models.ForeignKey(Entidad)
+    solicitud = models.PositiveIntegerField()
