cc274c18f3736457c0ddfcc33534b7102b49f949
karent Narvaez <karentnathalia@gmail.com>
2016-02-24 19:29:54 -0500

    cambio módelo competencia y visualización

--- registro_resultados/forms.py
+++ registro_resultados/forms.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 from django import forms
 from registro_resultados.models import *
-from coldeportes.utilities import adicionarClase, MyDateTimeWidget
+from coldeportes.utilities import adicionarClase, MyDateWidget
 
 class CompetenciaForm(forms.ModelForm):
     required_css_class = 'required'
@@ -10,17 +10,25 @@ class CompetenciaForm(forms.ModelForm):
         (1, "Tiempos"),
         (2, "Puntos"),
     )
-    tipo_registro = forms.ChoiceField(widget=forms.RadioSelect, choices=TIPOS_REGISTROS)
+    tipo_registro = forms.ChoiceField(widget=forms.RadioSelect, choices=TIPOS_REGISTROS, label='Registros De Competencia')
 
     def __init__(self, *args, **kwargs):
+        deporte_id = kwargs.pop('deporte_id',None)
         super(CompetenciaForm, self).__init__(*args, **kwargs)
         self.fields['disciplina_deportiva'] = adicionarClase(self.fields['disciplina_deportiva'], 'one')
+        self.fields['categoria'] = adicionarClase(self.fields['categoria'], 'one')
+        self.fields['categoria'].queryset = CategoriaDisciplinaDeportiva.objects.none()
+        self.fields['modalidad'].queryset = ModalidadDisciplinaDeportiva.objects.none()
+        self.fields['disciplina_deportiva'].queryset = TipoDisciplinaDeportiva.objects.all().order_by('descripcion')
+        if deporte_id:
+            self.fields['categoria'].queryset = CategoriaDisciplinaDeportiva.objects.filter(deporte=deporte_id).order_by('nombre')
+            self.fields['modalidad'].queryset = ModalidadDisciplinaDeportiva.objects.filter(deporte=deporte_id).order_by('nombre')
 
     class Meta:
         model = Competencia
         exclude = ()
         widgets = {
-            'fecha_competencia': MyDateTimeWidget(),
+            'fecha_competencia': MyDateWidget(),
         }
 
 class ParticipanteForm(forms.ModelForm):
