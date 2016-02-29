c97102fd182c6587e98dea09b0c6ac70218ce687
Daniel Correa <danielc.web@gmail.com>
2016-02-29 22:34:51 +0100

    avance de historico de solicitudes de escenarios (lado respuesta)

--- solicitudes_escenarios/respuesta/urls.py
+++ solicitudes_escenarios/respuesta/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('solicitudes_escenarios.respuesta.views',
-    #url(r'^crear$', 'generar_solicitud', name='generar_solicitud'),
+    url(r'^listar$', 'listar_solicitudes', name='listar_solicitudes'),
 
 )
--- solicitudes_escenarios/respuesta/views.py
+++ solicitudes_escenarios/respuesta/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from solicitudes_escenarios.respuesta.models import ListaSolicitudes
+from solicitudes_escenarios.solicitud.models import SolicitudEscenario
+from django.db import connection
 # Create your views here.
+@login_required
+def listar_solicitudes(request):
+    yo=request.tenant
+    lista = ListaSolicitudes.objects.all()
+    result = []
+    for l in lista:
+        entidad = l.entidad_solicitante
+        id_sol = l.solicitud
+        connection.set_tenant(entidad)
+        sol = SolicitudEscenario.objects.get(id=id_sol)
+        sol.entidad_solicitante = entidad
+        sol.codigo = sol.codigo_unico(entidad)
+        sol.escenarios_str = sol.escenarios_str()
+        result.append(sol)
+
+    connection.set_tenant(yo)
+    return render(request,'lista_solicitudes.html',{
+        'solicitudes': result
+    })
