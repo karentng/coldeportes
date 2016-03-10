b34cf3822c364d332086d9c0c75976edd8cd11be
juandgc <juan.garcia.certuche@correounivalle.edu.co>
2016-03-10 11:50:59 -0500

    Creación modelo evento

--- gestion_eventos/models.py
+++ gestion_eventos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Evento(models.Model):
+
+    titulo_evento = models.CharField()
+    fecha_inicio = models.DateField()
+    fecha_finalizacion = models.DateField()
+    fecha_inicio_preinscripcion = models.DateField()
+    fecha_finalizacion_preinscripcion = models.DateField()
+    imagen = models.ImageField()
+    video = models.CharField()
+    descripcion_evento = models.TextField()
+    costo_entrada = models.CharField()
+    cupo_participantes = models.PositiveIntegerField()
