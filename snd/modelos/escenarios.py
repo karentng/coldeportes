b54154b482295ed0aa06ecc57ef8b6c8f10267ff
MiltonLn <miltonln04@gmail.com>
2016-03-18 15:12:54 -0500

    fix escenario

--- snd/modelos/escenarios.py
+++ snd/modelos/escenarios.py
@@ -23,7 +23,7 @@ class Escenario(models.Model):
         ('ZR','ZONA RURAL'),
         ('ZU','ZONA URBANA'),
     )
-    nombre =  models.CharField(max_length=100,unique=True,help_text="asdasd")
+    nombre =  models.CharField(max_length=100,unique=True)
     razon_social = models.CharField(max_length=255, verbose_name="Razón social", blank=True)
     direccion = models.CharField(max_length=100, verbose_name='dirección')
     latitud = models.FloatField(null=True, blank=True)
