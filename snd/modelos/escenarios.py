aa061af5541c2d7a6391aed12c6e01c171b95e4e
Yalile <yalile06@gmail.com>
2016-03-28 14:49:24 -0500

    Se añadio un nuevo campo para identificar el tipo de suceso histórico

--- snd/modelos/escenarios.py
+++ snd/modelos/escenarios.py
@@ -174,9 +174,17 @@ class Mantenimiento(models.Model):
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
 class DatoHistorico(models.Model):
+    SUCESOS = (
+        ('adecuacion', 'ADECUACIONES'),
+        ('juego', 'JUEGO'),
+        ('cultural', 'EVENTO CULTURAL'),
+        ('inauguracion', 'INAUGURACIÓN'),
+        ('otros', 'OTROS'),
+    )
     escenario = models.ForeignKey(Escenario)
     fecha_inicio = models.DateField(verbose_name="fecha inicio del suceso histórico")
     fecha_fin = models.DateField(null=True, blank=True)
+    tipo_suceso = models.CharField(max_length=20, verbose_name="Tipo de suceso histórico", choices=SUCESOS)
     descripcion = models.TextField(max_length=1024, verbose_name="descripción del suceso histórico")
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
