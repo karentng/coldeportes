746a0ecd33a8bae6a62f485491e2458f3bf8755c
Daniel Correa <danielc.web@gmail.com>
2016-03-29 20:46:12 +0200

    Issue #507 resulto, imagenes y pdf permitido

--- static/js/validaciones/solicitud_escenario/wizard-adjunto-validations.js
+++ static/js/validaciones/solicitud_escenario/wizard-adjunto-validations.js
@@ -2,8 +2,10 @@ form = "#form-adjunto-solicitud";
 fields = {
             archivo: {
                 validators: {
-                    notEmpty: {
-                        message: 'Debe seleccionar un archvio para poder adicionarlo'
+                    file: {
+                        extension: 'png,jpg,jpeg,svg,pdf',
+                        message: 'Seleccione un archivo con alguno de los siguientes formatos (png, jpg, jpeg, svg, pdf) cuyo tamaño sea menor a 5MB',
+                        maxSize: 5242880, // 5MB: http://www.beesky.com/newsite/bit_byte.htm
                     }
                 }
             }
