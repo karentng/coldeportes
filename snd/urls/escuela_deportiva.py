7d9bc15a690fb1a0c26b48734f7d475f177324e3
juandgc <juan.garcia.certuche@correounivalle.edu.co>
2016-04-04 18:07:40 -0500

    Agregado funcionalidades al menú de gestionar mi entidad

--- snd/urls/escuela_deportiva.py
+++ snd/urls/escuela_deportiva.py
@@ -10,16 +10,16 @@ urlpatterns = patterns(
     'snd.views.escuela_deportiva',
 
     # urls sedes
-    url(r'^sedes/listar$', 'listar', name='escuela_deportiva_listar'),
-    url(r'^sedes/finalizar/(?P<edicion>\d+)$', 'finalizar', name='escuela_deportiva_finalizar'),
-    url(r'^sedes/ver/(\d+)/(\d+)$', 'ver', name='escuela_deportiva_ver'),
-    url(r'^sedes/desactivar/(\d+)$', 'desactivar_escuela_deportiva', name='escuela_deportiva_desactivar'),
+    url(r'^escuela-deportiva/sedes/listar$', 'listar', name='escuela_deportiva_listar'),
+    url(r'^escuela-deportiva/sedes/finalizar/(?P<edicion>\d+)$', 'finalizar', name='escuela_deportiva_finalizar'),
+    url(r'^escuela-deportiva/sedes/ver/(\d+)/(\d+)$', 'ver', name='escuela_deportiva_ver'),
+    url(r'^escuela-deportiva/sedes/desactivar/(\d+)$', 'desactivar_escuela_deportiva', name='escuela_deportiva_desactivar'),
 
     # urls participantes
-    url(r'^participantes/registrar', 'registrar_participante', name='registrar_participante'),
-    url(r'^participantes/listar$', 'listar_participante', name='listar_participante'),
+    url(r'^escuela-deportiva/participantes/registrar', 'registrar_participante', name='registrar_participante'),
+    url(r'^escuela-deportiva/participantes/listar$', 'listar_participante', name='listar_participante'),
 
     # urls wizard
-    url(r'^sedes/crear_editar/(?P<paso>\w+)/(?P<edicion>\d+)$', 'crear_editar', name='escuela_deportiva_crear_editar'),
-    url(r'^sedes/crear_editar/(?P<paso>\w+)/(?P<edicion>\d+)/(?P<escuela_deportiva_id>\d+)$', 'crear_editar', name='escuela_deportiva_crear_editar'),
+    url(r'^escuela-deportiva/sedes/crear_editar/(?P<paso>\w+)/(?P<edicion>\d+)$', 'crear_editar', name='escuela_deportiva_crear_editar'),
+    url(r'^escuela-deportiva/sedes/crear_editar/(?P<paso>\w+)/(?P<edicion>\d+)/(?P<escuela_deportiva_id>\d+)$', 'crear_editar', name='escuela_deportiva_crear_editar'),
 )
