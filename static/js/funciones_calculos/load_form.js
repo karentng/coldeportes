c5712cee7cb5c2d8d6ac9cd6b75b5f84e0e26e16
juandgc <juan.garcia.certuche@correounivalle.edu.co>
2016-04-26 14:30:32 -0500

    Arreglado div de carga en crear entidades

--- static/js/funciones_calculos/load_form.js
+++ static/js/funciones_calculos/load_form.js
@@ -2,7 +2,16 @@
  * Created by juandgc on 12/04/16.
  */
 
-$(loadForm).submit(function(e){
+$(btnForm).click(function(e){
+    console.log("antes validate");
+    $("#"+ loadForm).bootstrapValidator('validate');
+    if ($("div.form-group").hasClass("has-error")) {
+        console.log("dentro if");
+        e.preventDefault();
+        return false;
+    }
     $(".q-hide").removeClass("hide");
     $("div.q-hide").css({"opacity":"0.8", "z-index": "2000"});
+    console.log("dentro else");
+    document.getElementById(loadForm).submit();
 });
