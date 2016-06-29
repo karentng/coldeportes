1b3f24c0d2f77d59657b79f408fb718349f2b87a
Daniel <danielc.web@gmail.com>
2016-06-29 12:41:40 -0500

    Avance con servicios rest deportistas

--- api_interoperable/urls.py
+++ api_interoperable/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('api_interoperable.views',
-    url(r'^list-depor/$', 'deportista_list', name='get_depor'),
+    url(r'^deportistas/$', 'deportista_get_post', name='get_depor'),
+    url(r'^deportistas/(?P<pk>[0-9]+)$', 'deportista_detail_put_delete', name='get_depor_detail'),
 
 )
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api_interoperable/views.py
+++ api_interoperable/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse,JsonResponse
 from snd.models import Deportista
 from api_interoperable.models import DeportistaSerializable
 from rest_framework import status
@@ -6,12 +8,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from entidades.models import Entidad
 from django.db import connection
-from django.http import JsonResponse
 # Create your views here.
 
 @api_view(['GET', 'POST'])
-def deportista_list(request,format=None):
-    ent = Entidad.objects.get(schema_name='deporcali')
+def deportista_get_post(request,format=None):
+    ent = Entidad.objects.get(schema_name='club')
 
     if request.method == 'GET':
         connection.set_tenant(ent)
@@ -22,8 +23,33 @@ def deportista_list(request,format=None):
     elif request.method == 'POST':
         connection.set_tenant(ent)
         serializer = DeportistaSerializable(data=request.data)
+        print(request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        #print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT','DELETE'])
+def deportista_detail_put_delete(request,pk,format=None):
+    try:
+        deportista = Deportista.objects.get(pk=pk)
+    except Deportista.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DeportistaSerializable(deportista)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = DeportistaSerializable(deportista, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        deportista.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
