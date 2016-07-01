6193da9a33c09ab8a13f78b7a7dadaf9ef9b484a
Daniel <danielc.web@gmail.com>
2016-07-01 09:28:20 -0500

    Vistas materializadas en api rest

--- api_interoperable/urls.py
+++ api_interoperable/urls.py
@@ -1,10 +1,11 @@
 #-*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
+from api_interoperable import views
 
 urlpatterns = patterns('api_interoperable.views',
-    url(r'^deportistas/$', 'deportista_get_post', name='get_depor'),
-    url(r'^deportistas/(?P<pk>[0-9]+)$', 'deportista_detail_put_delete', name='get_depor_detail'),
+    url(r'^deportistas/$', views.DeportistasList.as_view(), name='get_depor'),
+    url(r'^deportistas/(?P<pk>[0-9]+)$', views.DeportistaDetail.as_view(), name='get_depor_detail'),
 
 )
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api_interoperable/views.py
+++ api_interoperable/views.py
@@ -6,50 +6,53 @@ from api_interoperable.models import DeportistaSerializable
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from entidades.models import Entidad
-from django.db import connection
+from rest_framework.views import APIView
+from django.http import Http404
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def deportista_get_post(request,format=None):
-    ent = Entidad.objects.get(schema_name='club')
+class DeportistasList(APIView):
+    """
+    Clase encargada de get y post de deportistas
+    """
 
-    if request.method == 'GET':
-        connection.set_tenant(ent)
+    def get(self,request,format=None):
         deportistas = Deportista.objects.all()
         serializer = DeportistaSerializable(deportistas, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        connection.set_tenant(ent)
+    def post(self,request,format=None):
         serializer = DeportistaSerializable(data=request.data)
-        print(request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        #print(serializer.errors)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT','DELETE'])
-def deportista_detail_put_delete(request,pk,format=None):
-    try:
-        deportista = Deportista.objects.get(pk=pk)
-    except Deportista.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class DeportistaDetail(APIView):
+    """
+    Clase encargada del put, delete y get personal
+    """
 
-    if request.method == 'GET':
+    def get_object(self,pk):
+        try:
+            return Deportista.objects.get(id=pk)
+        except Deportista.DoesNotExist:
+            raise Http404
+
+    def get(self,request,pk,format=None):
+        deportista = self.get_object(pk)
         serializer = DeportistaSerializable(deportista)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = DeportistaSerializable(deportista, data=request.data)
+    def put(self,request, pk, format=None):
+        deportista = self.get_object(pk)
+        serializer = DeportistaSerializable(deportista,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        deportista.delete()
+    def delete(self,request,pk, format=None):
+        deportista = self.get_object(pk)
+        deportista.estado = 1
+        deportista.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
