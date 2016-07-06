47466877662bba8f2e4d7b9b760d83df162c800c
Daniel <danielc.web@gmail.com>
2016-07-05 19:50:51 -0500

    Soporte para composicon corporal y historial deportivo, metodo de verificacion de entidad mejorado, atributo readonly, metodo DELETE personalizado mejorado

--- api_interoperable/urls.py
+++ api_interoperable/urls.py
@@ -4,10 +4,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api_interoperable import views
 
 urlpatterns = patterns('api_interoperable.views',
-    url(r'^deportistas/$', views.DeportistasList.as_view(), name='get_depor'),
-    url(r'^deportistas/(?P<pk>[0-9]+)$', views.DeportistaDetail.as_view(), name='get_depor_detail'),
-
-    url(r'^auth/',include('rest_framework.urls',namespace='rest_framework')),
+    #url(r'^$', views.api_root, name='rest_root'),
+    url(r'^deportistas/$', views.DeportistasList.as_view()),
+    url(r'^deportistas/(?P<pk>[0-9]+)$', views.DeportistaDetail.as_view()),
+    url(r'^deportistas/corporal$', views.ComposcionCorporalList.as_view()),
+    url(r'^deportistas/corporal/(?P<pk>[0-9]+)$', views.ComposcionCorporalDetail.as_view()),
+    url(r'^deportistas/historialdeportivo', views.HistorialDeportivolList.as_view()),
+    url(r'^deportistas/historialdeportivo/(?P<pk>[0-9]+)$', views.HistorialDeportivoDetail.as_view()),
+    url(r'^api-auth/',include('rest_framework.urls',namespace='rest_framework')),
 
 )
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api_interoperable/views.py
+++ api_interoperable/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from snd.models import Deportista
-from api_interoperable.models import DeportistaSerializable
+from snd.models import Deportista,ComposicionCorporal,HistorialDeportivo,InformacionAcademica,InformacionAdicional,HistorialLesiones
+from api_interoperable.models import DeportistaSerializable,ComposicionCorporalSerializable,HistorialDeportivoSerializable,InformacionAcademicaSerializable,HistorialLesionesSerializable,InformacionAdicionalSerializable
 from rest_framework import status
 from rest_framework.response import Response
 from django.http import Http404
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 # Create your views here.
+"""@api_view(['GET'])
+def api_root(request, format=None):
+    return Response(
+        {
+            'deportistas': reverse('deportista-list',request=request,format=format)
+        }
+    )
+"""
 class AddChangePermission(permissions.BasePermission):
     """
     Clase que define el permiso para editar , crear y eliminar deportistas
@@ -18,7 +28,7 @@ class AddChangePermission(permissions.BasePermission):
             return request.user.has_perm("snd.add_deportista")
         return True
 
-
+#API REST para modelo Deportista
 class DeportistasList(generics.ListCreateAPIView):
     """
     Clase encargada de get y post de deportistas
@@ -32,11 +42,8 @@ class DeportistasList(generics.ListCreateAPIView):
         Permite validar si la entidad proveniente del deportista corresponde a la del request
         :param serializer: clase encargada de serializar el objeto
         """
-        entidad_registrada = serializer.data["entidad"]
-        entidad_request = self.request.tenant.id
-        if entidad_registrada != entidad_request:
-            raise serializer.ValidationError("Entidad no corresponde")
-        serializer.save()
+        serializer.save(entidad=self.request.tenant)
+
 
 class DeportistaDetail(generics.RetrieveUpdateAPIView):
     """
@@ -53,4 +60,44 @@ class DeportistaDetail(generics.RetrieveUpdateAPIView):
             raise Http404
         deportista.estado = 1
         deportista.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#API REST para modelo Composicion Corporal
+class ComposcionCorporalList(generics.ListCreateAPIView):
+    """
+        Clase encargada de get y post de composicion corporal de deportista
+        """
+    queryset = ComposicionCorporal.objects.all()
+    serializer_class = ComposicionCorporalSerializable
+    permission_classes = (permissions.IsAuthenticated, AddChangePermission,)
+
+class ComposcionCorporalDetail(generics.RetrieveUpdateAPIView):
+    """
+    Clase encargada del put, delete y get personal de composicion corporal de deportista
+    """
+    queryset = ComposicionCorporal.objects.all()
+    serializer_class = ComposicionCorporalSerializable
+    permission_classes = (permissions.IsAuthenticated,AddChangePermission,)
+
+    def perform_destroy(self,instance):
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#API REST para modelo historial deportivo
+class HistorialDeportivolList(generics.ListCreateAPIView):
+    """
+        Clase encargada de get y post de composicion corporal de deportista
+        """
+    queryset = HistorialDeportivo.objects.all()
+    serializer_class = HistorialDeportivoSerializable
+    permission_classes = (permissions.IsAuthenticated, AddChangePermission,)
+
+class HistorialDeportivoDetail(generics.RetrieveUpdateAPIView):
+    """
+    Clase encargada del put, delete y get personal de composicion corporal de deportista
+    """
+    queryset = HistorialDeportivo.objects.all()
+    serializer_class = HistorialDeportivoSerializable
+    permission_classes = (permissions.IsAuthenticated,AddChangePermission,)
+
+    def perform_destroy(self, instance):
         return Response(status=status.HTTP_204_NO_CONTENT)
